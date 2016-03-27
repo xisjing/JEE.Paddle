556c4b11875275fc4729bdc148c8fc2ca95fc868
sijing <xisijing@gmail.com>
2016-03-28 00:16:05 +0200

    añadir deleteTrainingPlayer() y registerPlayer() a TrainingDao

--- src/main/java/data/daos/TrainingDao.java
+++ src/main/java/data/daos/TrainingDao.java
@@ -20,14 +20,26 @@ public interface TrainingDao extends JpaRepository<Reserve, Integer> {
 	//create Training 
 	@Modifying
     @Transactional 
-    @Query("insert into(training.court,training.user,training.trainingDate) Training training values (?1,?2,?3)" )
-	void createTraining(Court court, User user, Calendar trainingDate);
+    @Query("insert into(training.court,training.trainer,training.player,training.trainingDate) Training training values (?1,?2,?3,?4)" )
+	void createTraining(Court court, User trainer, User player, Calendar trainingDate);
 	
 	//delete Training
 	@Modifying
     @Transactional 
-    @Query("delete from Training training where training.user = ?1")
+    @Query("delete from Training training where training.trainer = ?1")
     void deleteTraining(User trainer);
+	
+	//register Training
+	@Modifying
+    @Transactional 
+    @Query("update training.player = ?1 from Training training where training.id = ?2 ")
+    void registerPlayer(User player, String id);
+	
+	//delete TrainingPlayer
+	@Modifying
+    @Transactional 
+    @Query("update training.player = '' from Training training where training.trainer = ?1 and training.id = ?2 ")
+    void deleteTrainingPlayer(User trianer, String id);
 	
 	
 
