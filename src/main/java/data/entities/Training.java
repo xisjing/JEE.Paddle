585649d310b54e3f52c5c172c4a00e1a753370c2
sijing <xisijing@gmail.com>
2016-03-28 19:10:00 +0200

    modificar Entity Training, pero no se ejecuta correctamente

--- src/main/java/data/entities/Training.java
+++ src/main/java/data/entities/Training.java
@@ -2,10 +2,17 @@ package data.entities;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
 
 @Entity
 public class Training {
@@ -14,38 +21,65 @@ public class Training {
 	@GeneratedValue
     private int id;
 	
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "players")
+    private List<User> players;
+		
+	@ManyToOne
+	@JoinColumn
 	private User trainer;
-	
-	private User player;
-	
+		
+	@ManyToOne
+	@JoinColumn
 	private Court court;
 	
 	private Calendar trainingDate;
+
+	public Training(){
+		
+	}
 	
-	public Training(Court court, User player, User trainer, Calendar trainingDate) {
+	public Training(Court court, List<User> players, User trainer, Calendar trainingDate) {
         this.court = court;
-        this.player = player;
+        this.players = players;
         this.trainer = trainer;
         this.trainingDate = trainingDate;
     }
 	
-    public int getId(){
-    	return id;
-    }
-    
-   
+	public int getId() {
+		return id;
+	}
+	
+	public User getTrainer() {
+		return trainer;
+	}
+
+	public void setTrainer(User trainer) {
+		this.trainer = trainer;
+	}
+
+	public List<User> getPlayers() {
+		return players;
+	}
+
+	public void setPlayers(List<User> players) {
+		this.players = players;
+	}
+	
 	public Court getCourt() {
 		return court;
 	}
 
+	public void setCourt(Court court) {
+		this.court = court;
+	}
+	
 	public Calendar getTrainingDate() {
 		return trainingDate;
 	}
-	
-	 @Override
-	 public int hashCode() {
-	        return id;
-	    }
+
+	public void setTrainingDate(Calendar trainingDate) {
+		this.trainingDate = trainingDate;
+	}
 
 	 @Override
 	  public boolean equals(Object obj) {
@@ -64,24 +98,12 @@ public class Training {
 	    @Override
 	    public String toString() {
 	        String time = new SimpleDateFormat("HH:00 dd-MMM-yyyy ").format(trainingDate.getTime());
-	        return "Training [id=" + id + ", courtId=" + court.getId() + ", player =" + player.getId() + ", trainer =" + trainer.getId() + ", date=" + time + "]";
+	        return "Training [id=" + id + ", courtId=" + court.getId() + ", players =" + players.get(id)  + ", trainer =" + trainer.getId() + ", date=" + time + "]";
 	    }
 
-		public User getTrainer() {
-			return trainer;
-		}
+		
 
-		public void setTrainer(User trainer) {
-			this.trainer = trainer;
-		}
-
-		public User getPlayer() {
-			return player;
-		}
-
-		public void setPlayer(User player) {
-			this.player = player;
-		}
+		
 
 	}
 
